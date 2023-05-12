93685a64a14ddfcc7d0515800814ddb1a77e1683
SimoLomba53 <simolomba253@gmail.com>
2023-05-12 11:20:16 +0200

    Navbar

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,11 @@
 <script setup>
 import HelloWorld from './components/HelloWorld.vue'
+import Homepage from './components/Homepage.vue';
 </script>
 
 <template>
-  <HelloWorld msg="Vite + Vue" />
+  <HelloWorld />
+  <Homepage/>
 </template>
 
 <style scoped>
