cda3e82de4f61b6b9d8c7917b746a117b6cb5c41
SimoLomba53 <simolomba253@gmail.com>
2023-05-17 11:16:58 +0200

    colori nuovi e java effect

--- src/main.js
+++ src/main.js
@@ -5,3 +5,19 @@ import "bootstrap/dist/css/bootstrap.css";
 
 createApp(App).mount('#app')
 import "bootstrap/dist/js/bootstrap.js";
+
+//scroll animation
+
+const observer = new IntersectionObserver((entries)=>{
+    entries.forEach((entry)=>{
+        console.log(entry)
+        if(entry.isIntersecting){
+            entry.target.classList.add('show');
+        }else{
+            entry.target.classList.remove('show');
+        }
+    });
+});
+
+const introElements=document.querySelectorAll('.intro');
+introElements.forEach((el) => observer.observe(el));
