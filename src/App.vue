67d2b70c1423fb4e6a7fb1b1b16983e57349b995
RiccardoPietrobon <riccardopietrobon.sanso@gmail.com>
2023-05-17 12:16:01 +0200

    ok

--- src/App.vue
+++ src/App.vue
@@ -2,14 +2,12 @@
 import HelloWorld from './components/HelloWorld.vue'
 import Homepage from './components/Homepage.vue';
 import FooterComponent from './components/FooterComponent.vue';
-import ApartmentsList from './components/ApartmentsList.vue';
 </script>
 
 <template>
   <HelloWorld />
   <Homepage />
   <FooterComponent />
-  <ApartmentsList />
 </template>
 
 <style scoped></style>
--- src/components/ApartmentsList.vue
+++ src/components/ApartmentsList.vue
@@ -13,7 +13,7 @@ export default {
             axios
                 .get('http://127.0.0.1:8000/api/apartments')
                 .then((response) => {
-                    this.apartments = response.data;
+                    this.apartments = response.data.apartments;
                 })
         }
     },
