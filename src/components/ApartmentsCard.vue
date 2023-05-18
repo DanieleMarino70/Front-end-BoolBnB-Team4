8966c2a18dd323176a2bf13752018f8f46c4fd9f
SimoLomba53 <simolomba253@gmail.com>
2023-05-18 11:34:08 +0200

    dettagli apartment

--- src/components/ApartmentsCard.vue
+++ src/components/ApartmentsCard.vue
@@ -3,20 +3,55 @@ export default {
   props: {
     title: String,
     address: String,
+    squaremeters:String,
     image: Image,
   },
 };
 </script>
 
 <template>
-  <div class="card" style="width: 18rem">
-    <img class="card-img-top" :src="image" alt="Card image cap" />
-    <div class="card-body">
-      <h5 class="card-title">{{ title }}</h5>
-      <p class="card-text">{{ address }}</p>
-      <a href="#" class="btn btn-primary">MOSTRAMI L'APPARTAMENTO</a>
+  <div class="apartmentcard card p-3 bg-white" style="width: 18rem ; height: 26rem;"  >
+    <img class="card-img-top" :src="image" alt="Card image cap" style="width:18vw; height:20vh;"  />
+    <div class="card-body ">
+      <h5 class="card-title fw-bold">{{ title }}</h5>
+      <p class="card-text text-muted">{{ address }}</p>
+      <a href="#" class="text-center">CONTATTA HOST</a>
+      
     </div>
   </div>
 </template>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.card-body{
+  display: flex;
+  flex-grow: 0;
+  flex-direction: column;
+  justify-content: end;
+}
+
+.apartmentcard{
+  border:transparent;
+  height: 100%;
+}
+
+.apartmentcard img{
+  border-radius: 10px;
+}
+
+
+.card-body a {
+  text-decoration: none;
+  display: inline-block;
+  background-color: #e4baa0;
+  color: white;
+  font-size: 14px;
+  border: 2px solid white;
+  padding: 10px;
+  border-radius: 50px;
+  transition: 0.5s;
+}
+.card-body a:hover {
+  background-color: white;
+  color: #e4baa0;
+}
+</style>
--- src/pages/ApartmentsList.vue
+++ src/pages/ApartmentsList.vue
@@ -23,17 +23,34 @@ export default {
 </script>
 
 <template>
+  <link rel="preconnect" href="https://fonts.googleapis.com" />
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+  <link
+    href="https://fonts.googleapis.com/css2?family=Fjalla+One&display=swap"
+    rel="stylesheet"
+  />
+  <link rel="stylesheet" href="style.css" />
+  <link rel="stylesheet" href="main.js" />
+  <section>
   <div class="container my-4">
-    <div class="row row-cols-3">
+    <div class="row row-cols-3 d-flex align-items-center justify-content-center gap-5">
       <ApartmentsCard
         class="col"
         v-for="apartment in apartments"
         :title="apartment.title"
         :address="apartment.address"
         :image="apartment.image"
+        :squaremeters="apartment.square_meters"
       />
     </div>
   </div>
+  </section>
 </template>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+ template{
+  background-color: #e4baa0;
+ }
+
+
+</style>
