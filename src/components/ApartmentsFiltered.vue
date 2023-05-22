9b3543024e8efcfc5ca85e42949102b35690b23f
DanieleMarino70 <danielaotitan@gmail.com>
2023-05-22 11:26:40 +0200

    add: list of filtetered apartments

---
+++ src/components/ApartmentsFiltered.vue
@@ -0,0 +1,21 @@
+<script>
+import ApartmentsCard from "../components/ApartmentsCard.vue";
+
+export default {
+    props: {
+        apartments: Array,
+    },
+    components: { ApartmentsCard },
+};
+</script>
+
+<template>
+    <div class="container my-4">
+        <div class="row row-cols-3 d-flex align-items-center justify-content-center gap-5">
+            <ApartmentsCard class="col" v-for="apartment in apartments" :id="apartment.id" :title="apartment.title"
+                :address="apartment.address" :image="apartment.image" :squaremeters="apartment.square_meters" />
+        </div>
+    </div>
+</template>
+
+<style lang="scss" scoped></style>
