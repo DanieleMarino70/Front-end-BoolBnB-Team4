bc0a0b7dfc22408c80e156f54444383213b0e20f
SimoLomba53 <simolomba253@gmail.com>
2023-05-22 11:40:42 +0200

    show pronta

--- src/pages/ApartmentShow.vue
+++ src/pages/ApartmentShow.vue
@@ -4,6 +4,9 @@ import axios from "axios";
 import ApartmentsCard from "../components/ApartmentsCard.vue";
 import MessageForm from "../components/MessageForm.vue";
 import ApartmentsAllVue from "../components/ApartmentsAll.vue";
+import { parseQuery } from "vue-router";
+import { onMounted } from "vue";
+import AOS from "aos";
 
 export default {
   data() {
@@ -18,19 +21,89 @@ export default {
       });
   },
 
-  components: { ApartmentsCard, MessageForm },
+  components: { ApartmentsCard, MessageForm, parseQuery },
 };
+AOS.init();
 </script>
 
 <template>
+  
+  <link rel="preconnect" href="https://fonts.googleapis.com" />
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+  <link
+    href="https://fonts.googleapis.com/css2?family=Fjalla+One&display=swap"
+    rel="stylesheet"
+  />
+
   <div class="container">
     <div class="row justify-content-center">
-      <div class="col">
-        <ul>
-          <li></li>
+      <div class="col contcard p-4">
+        <ul class="d-flex flex-row">
+          <div>
+          <li>
+            <div class="d-flex flex-row gap-3">
+              <p class="text-white">ID:</p>
+              <p class="text-dark">{{ apartment.id }}</p>
+            </div>
+          </li>
+          <li>
+            <div class="d-flex flex-row gap-3">
+              <p class="text-white">Nome Appartamento:</p>
+              <p class="text-dark">{{ apartment.title }}</p>
+            </div>
+          </li>
+          <li>
+            <div class="d-flex flex-row gap-3">
+              <p class="text-white">Indirizzo:</p>
+              <p class="text-dark">{{ apartment.address }}</p>
+            </div>
+          </li>
+          <li>
+            <div class="d-flex flex-row gap-3">
+              <p class="text-white">Numero di stanze:</p>
+              <p class="text-dark">{{ apartment.rooms }}</p>
+            </div>
+          </li>
+          <li>
+            <div class="d-flex flex-row gap-3">
+              <p class="text-white">Numero di stanze:</p>
+              <p class="text-dark">{{ apartment.rooms }}</p>
+            </div>
+          </li>
+          <li>
+            <div class="d-flex flex-row gap-3">
+              <p class="text-white">Numero di stanze:</p>
+              <p class="text-dark">{{ apartment.rooms }}</p>
+            </div>
+          </li>
+           <li>
+            <div class="d-flex flex-row gap-3">
+              <p class="text-white">Metri quadri:</p>
+              <p class="text-dark">{{ apartment.square_meters }}</p>
+            </div>
+          </li>
+          <li>
+            <div class="d-flex flex-row gap-3">
+              <p class="text-white">Visibilità:</p>
+              <p class="text-dark">{{ apartment.visibility }}</p>
+            </div>
+          </li>
+          <li>
+            <div class="d-flex flex-column gap-3 ">
+              <p class="text-white">Descrizione:</p>
+              <p class="text-dark">{{ apartment.description }}</p>
+            </div>
+          </li>
+          </div>
+          <div>
+          <li class="imagelist">
+            <img :src="apartment.image" alt="">
+          </li>
+          </div>
+          
           <!-- <li>{{ apartment.description }}</li>
           <li>{{ apartment.rooms }}</li> -->
-          <li>{{ apartment.id }}</li>
+          
         </ul>
         <MessageForm :apartment_id="apartment.id" />
       </div>
@@ -38,4 +111,31 @@ export default {
   </div>
 </template>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+  .imagelist img{
+    width: 35vw;
+    border-radius: 10px;
+    transition: 0.5s;
+    
+  }
+
+   .imagelist img:hover{
+     transform: scale(1.03);
+    
+  }
+
+  li{
+    list-style: none
+  }
+
+
+
+  .contcard{
+    background-color:#e4baa0;
+    font-family:'Fjalla One', sans-serif; 
+    border-radius: 10px;
+    
+  }
+
+  
+</style>
